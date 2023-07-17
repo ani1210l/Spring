bb3dc1dfb829b74d98fed0ee573ada535d4692f9
ani1210l <carlisanahileon1210@gmail.com>
2023-07-17 10:51:31 -0500

    Up

---
+++ src/main/resources/import.sql
@@ -0,0 +1,14 @@
+insert into cliente(nombre,apellido,email) values ('Franklin', 'Tapia','franklintapia@gmail.com');
+insert into categoria(descripcion) values ('franklin Santiago Tapia Vasquez estudiante del tecnologico del azuay edad 20 años estado civil en relacion.');
+
+insert into cliente(nombre,apellido,email) values ('Damian ', 'Paredes','Damianparedes@gmail.com');
+insert into categoria(descripcion) values ('Jairo Damian Paredes Jimenez estudiante del tecnologico del azuay edad 21 años estado civil complicado. ');
+
+insert into cliente(nombre,apellido,email) values ('Victor ', 'chogllo','victorchogllo@gmail.com');
+insert into categoria(descripcion) values ('Victor Andres Chogllo Idrovo estudiante del Tecnologico del azuay edad 19 años estado civil en una relacion.');
+ 
+
+
+
+
+
